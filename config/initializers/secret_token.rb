4b4189726611eb6e06e9d522e6ccecd2639eb059
aloha <ravib@alohatechnologydev.com>
2018-03-22 17:22:40 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mahesh::Application.config.secret_key_base = '4a62009d2ad8ce5de4e32a96fe93692a145ab68cb248e3e0a3170c435ffbb3c2b8452c52eb3ea2f4cc8d0ef74e288b109ab7b1b463210b7d3222201f25e04df5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mahesh::Application.config.session_store :cookie_store, key: '_mahesh_session'
